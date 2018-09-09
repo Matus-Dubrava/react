100fdcbd1d5853c83b90c8d154f049d3fa56e17e
matus dubrava <matusdubrava@gmail.com>
2018-09-09 17:10:12 +0200

    added props to Person component

--- tutorials/person-app/src/App.js
+++ tutorials/person-app/src/App.js
@@ -6,9 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi , I'am a React App</h1>
+        <h1>Hi, I am a React App</h1>
         <p>This is really working</p>
-        <Person />
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="32" />
+        <Person name="Sue" age="42" />
       </div>
     );
   }
--- tutorials/person-app/src/components/Person/Person.js
+++ tutorials/person-app/src/components/Person/Person.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Person.css';
 
-const Person = () => {
-    return (
-        <p className="Person">
-            I'm a Person and I am {Math.floor(Math.random() * 30)} yeas old.
-        </p>
-    );
+const Person = (props) => {
+  return (
+      <p 
+				className="Person"
+      >
+				I am {props.name} and I am {props.age} yeas old.
+			</p>
+	);
 }
 
 export default Person;
