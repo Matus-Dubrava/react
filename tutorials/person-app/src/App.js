f365e4acc21dc8d1482f38bd6cd560ec4cd145a7
matus dubrava <matusdubrava@gmail.com>
2018-09-09 18:28:51 +0200

    added state and input field to Person component

--- tutorials/person-app/src/App.js
+++ tutorials/person-app/src/App.js
@@ -3,14 +3,47 @@ import './App.css';
 import Person from './components/Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 32 },
+      { name: 'Sue', age: 42 }
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    const persons = [...this.state.persons];
+    const max = {...persons[0]};
+    max.name = newName;
+    persons[0] = max;
+    this.setState({ persons });
+  };
+
+  nameChangedHandler = (event) => {
+    const persons = [...this.state.persons];
+    const manu = {...persons[1]};
+    manu.name = event.target.value;
+    persons[1] = manu;
+    this.setState({ persons });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really working</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="32" />
-        <Person name="Sue" age="42" />
+        <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          click={this.switchNameHandler.bind(this, 'Max!!!')}
+          changed={this.nameChangedHandler}
+          age={this.state.persons[1].age} />
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
--- tutorials/person-app/src/components/Person/Person.js
+++ tutorials/person-app/src/components/Person/Person.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Person.css';
 
-const Person = (props) => {
-  return (
-      <p 
-				className="Person"
-      >
-				I am {props.name} and I am {props.age} yeas old.
-			</p>
-	);
+class Person extends Component {
+	state = {
+		inputValue: ''
+	};
+
+	inputChangeHandler = (event) => {
+		this.setState({ inputValue: event.target.value });
+	};
+
+	render() {
+		return (
+			<div>
+				<p 
+					onClick={this.props.click}
+					className="Person" >
+					I am {this.props.name} and I am {this.props.age} yeas old.
+				</p>
+				<input 
+					type="text"
+					value={this.state.inputValue}
+					onChange={(event) => {
+						this.inputChangeHandler(event);
+						this.props.changed(event);
+					}} />
+			</div>
+		);
+	}
 }
 
 export default Person;
