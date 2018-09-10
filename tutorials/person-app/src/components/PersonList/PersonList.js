0365ac550aa2b74f200e575d6948867093a20c6f
matus dubrava <matusdubrava@gmail.com>
2018-09-10 18:24:35 +0200

    corrected indentation

--- tutorials/person-app/src/components/PersonList/PersonList.js
+++ tutorials/person-app/src/components/PersonList/PersonList.js
@@ -3,35 +3,35 @@ import './PersonList.css';
 import Person from './Person/Person';
 
 class PersonList extends PureComponent {
-  componentWillMount() {
-    console.log('[Persons.js] inside componentWillMount');
-  }
+	componentWillMount() {
+		console.log('[Persons.js] inside componentWillMount');
+	}
 
-  componentDidMount() {
-    console.log('[Persons.js] inside componentDidMount');
-  }
+	componentDidMount() {
+		console.log('[Persons.js] inside componentDidMount');
+	}
 
-  componentWillReceiveProps(nextProps) {
-    console.log('[UPDATE Persons.js] inside componentWillReceiveProps', nextProps)
-  }
+	componentWillReceiveProps(nextProps) {
+		console.log('[UPDATE Persons.js] inside componentWillReceiveProps', nextProps)
+	}
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   console.log('[UPDATE Persons.js] inside shouldComponentUpdate', nextProps, nextState);
-  //   return nextProps.persons !== this.props.persons ||
-  //     nextProps.deletePersonHandler !== this.props.deletePersonHandler ||
-  //     nextProps.nameChangedHandler !== this.props.nameChangedHandler;
-  // }
+	// shouldComponentUpdate(nextProps, nextState) {
+	//   console.log('[UPDATE Persons.js] inside shouldComponentUpdate', nextProps, nextState);
+	//   return nextProps.persons !== this.props.persons ||
+	//     nextProps.deletePersonHandler !== this.props.deletePersonHandler ||
+	//     nextProps.nameChangedHandler !== this.props.nameChangedHandler;
+	// }
 
-  componentWillUpdate(nextProps, nextState) {
-    console.log('[UPDATE Persons.js] inside componentWillUpdate', nextProps, nextState);
-  }
+	componentWillUpdate(nextProps, nextState) {
+		console.log('[UPDATE Persons.js] inside componentWillUpdate', nextProps, nextState);
+	}
 
-  componentDidUpdate() {
-    console.log('[UPDATE Persons.js] inside componentDidUpdate');
-  }
+	componentDidUpdate() {
+		console.log('[UPDATE Persons.js] inside componentDidUpdate');
+	}
 
 	render() {
-    console.log('[Persons.js] inside render');
+		console.log('[Persons.js] inside render');
 
 		const persons = this.props.persons.map((pers, i) => (
 			<Person 
@@ -42,12 +42,12 @@ class PersonList extends PureComponent {
 				deletePersonHandler={this.props.deletePersonHandler}
 				nameChangedHandler={this.props.nameChangedHandler} />	
 		));
-    
-    return (
-      <ul className="PersonList">
-        {persons}
-      </ul>
-    );
+		
+		return (
+			<ul className="PersonList">
+				{persons}
+			</ul>
+		);
 	}
 }
 
