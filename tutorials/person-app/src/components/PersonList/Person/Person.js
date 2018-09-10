f89d81bf72c9f9d15b73d0c1577ca227bdd62b50
matus dubrava <matusdubrava@gmail.com>
2018-09-10 18:21:08 +0200

    corrected indentataion

--- tutorials/person-app/src/components/PersonList/Person/Person.js
+++ tutorials/person-app/src/components/PersonList/Person/Person.js
@@ -4,11 +4,11 @@ import classes from './Person.css';
 import WithClass from '../../hoc/WithClass';
 
 class Person extends Component {
-	state = {
-		inputValue: this.props.name
-	};
+  state = {
+    inputValue: this.props.name
+  };
 
-	componentWillMount() {
+  componentWillMount() {
     console.log('[Person.js] inside componentWillMount');
   }
 
@@ -16,29 +16,29 @@ class Person extends Component {
     console.log('[Person.js] inside componentDidMount');
   }
 
-	inputChangeHandler = (event) => {
-		this.setState({ inputValue: event.target.value });
-		this.props.nameChangedHandler(event.target.value, this.props.id);
-	};
+  inputChangeHandler = (event) => {
+    this.setState({ inputValue: event.target.value });
+    this.props.nameChangedHandler(event.target.value, this.props.id);
+  };
 
-	render() {
+  render() {
     console.log('[Person.js] inside render');
 
-		return (
+    return (
       <WithClass classes={classes.Person}>
-				<p 
-					onClick={this.props.deletePersonHandler.bind(this, this.props.id)} >
-					I am {this.props.name} and I am {this.props.age} yeas old.
-				</p>
-				<input 
-					type="text"
-					value={this.state.inputValue}
-					onChange={(event) => {
-						this.inputChangeHandler(event);
-					}} />
+        <p 
+          onClick={this.props.deletePersonHandler.bind(this, this.props.id)} >
+          I am {this.props.name} and I am {this.props.age} yeas old.
+        </p>
+        <input 
+          type="text"
+          value={this.state.inputValue}
+          onChange={(event) => {
+            this.inputChangeHandler(event);
+          }} />
       </WithClass>
     );
-	} 
+  } 
 }
 
 export default Person;
