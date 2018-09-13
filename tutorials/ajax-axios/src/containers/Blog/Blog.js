36f9b6c8e3db751fe5f4c9b0c28213ce266f5859
matus dubrava <matusdubrava@gmail.com>
2018-09-13 20:40:48 +0200

    added Link's to project

--- tutorials/ajax-axios/src/containers/Blog/Blog.js
+++ tutorials/ajax-axios/src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -11,14 +12,21 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 <Route
-                    render={() => <h1>Home</h1>} 
+                    component={Posts} 
                     path='/' exact />
+                <Route
+                    component={NewPost} 
+                    path='/new-post' />
             </div>
         );
     }
