ab38980c200a0b08853dbaabe3e677dc0fc1393c
matus dubrava <matusdubrava@gmail.com>
2018-09-13 18:20:24 +0200

    added react-router

--- tutorials/ajax-axios/src/containers/Blog/Blog.js
+++ tutorials/ajax-axios/src/containers/Blog/Blog.js
@@ -1,65 +1,21 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../components/axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then((response) => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map((post) => {
-                    return {...post, author: 'Max' };
-                });
-                this.setState({ posts: updatedPosts });
-            })
-            .catch((err) => {
-                this.setState({ error: true });
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
-    }
-
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map((post) => {
-                return (
-                    <Post 
-                        clicked={this.postSelectedHandler.bind(this, post.id)}
-                        author={post.author}
-                        key={post.id}
-                        title={post.title} />
-                );
-            });
-        }
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost 
-                        title={true}
-                        content={true}
-                        id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Posts />
             </div>
         );
     }
