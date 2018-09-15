d14cbf0ad199caa2fa2c0b2681d73b2451b4cc8d
Sklep <matusdubrava@gmail.com>
2018-09-15 13:09:48 +0200

    added lazy loading for newPost component (asyncComponent)

--- tutorials/ajax-axios/src/containers/Blog/Blog.js
+++ tutorials/ajax-axios/src/containers/Blog/Blog.js
@@ -3,11 +3,15 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const asyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -38,7 +42,7 @@ class Blog extends Component {
                     {this.state.auth 
                         ? (
                             <Route
-                                component={NewPost} 
+                                component={asyncNewPost} 
                                 path='/new-post' exact />
                         )
                         : null}
