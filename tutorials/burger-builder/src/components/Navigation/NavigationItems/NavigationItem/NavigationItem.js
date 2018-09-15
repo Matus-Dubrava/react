2c3562807ec064f4c3a50a2c2d990d7a9f2ec162
Sklep <matusdubrava@gmail.com>
2018-09-15 21:01:35 +0200

    added ReactRouter and starter routing code

--- tutorials/burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ tutorials/burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItem.css';
 
 const navigationItem = (props) => {
   return (
     <li className={classes.NavigationItem}>
-      <a 
-        className={props.active ? classes.active : null}
-        href={props.link} >
-        {props.children}</a>
+      <NavLink
+        activeClassName={classes.active}
+        to={props.link} >
+        {props.children}</NavLink>
     </li>
   );
 };
