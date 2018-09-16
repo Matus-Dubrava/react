964cacbc38ae157962a737d96301cac46a67b285
Sklep <matusdubrava@gmail.com>
2018-09-16 12:18:44 +0200

    added form validation logic

--- tutorials/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ tutorials/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       street: {
         elementType: 'input',
@@ -23,7 +27,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Street'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       zipCode: {
         elementType: 'input',
@@ -31,7 +39,13 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'ZIP CODE'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false
       },
       country: {
         elementType: 'input',
@@ -39,7 +53,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       email: {
         elementType: 'email',
@@ -47,7 +65,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your E-Mail'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -62,6 +84,26 @@ class ContactData extends Component {
     },
     loading: false,
     totalPrice: 0
+  }
+
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = isValid && value.trim() !== '';
+    }
+
+    if (rules.minLength) {
+      isValid = isValid && value.length >= rules.minLength;
+    }
+
+    if (rules.maxLength) {
+      isValid = isValid && value.length <= rules.maxLength;
+    }
+
+
+
+    return isValid;
   }
 
   orderHandler = (event) => {
@@ -93,14 +135,16 @@ class ContactData extends Component {
   inputChangedHandler(event, inputId) {
     const updatedOrderForm = {
       ...this.state.orderForm
-    }
+    };
 
     const updatedFormElement = {
       ...updatedOrderForm[inputId]
-    }
+    };
 
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[inputId] = updatedFormElement;
+    console.log(updatedFormElement);
     this.setState({ orderForm: updatedOrderForm });
   }
 
