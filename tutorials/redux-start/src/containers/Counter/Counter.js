54fe96b6922fe5dfec4baa3ad5fbe808df8a1c8d
matus dubrava <matusdubrava@gmail.com>
2018-09-17 01:02:34 +0200

    added counter store array

--- tutorials/redux-start/src/containers/Counter/Counter.js
+++ tutorials/redux-start/src/containers/Counter/Counter.js
@@ -13,6 +13,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter.bind(this, 10)} />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter.bind(this, 10)} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map((res) => {
+                        return <li key={res.id} onClick={this.props.onDeleteResult.bind(this, res.id)}>{res.value}</li>
+                    })}
+                </ul>
             </div>
         );
     }
@@ -20,7 +27,8 @@ class Counter extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     }
 };
 
@@ -30,6 +38,8 @@ const mapDispatchToProps = (dispatch) => {
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: (value) => dispatch({type: 'ADD', value}),
         onSubtractCounter: (value) => dispatch({type: 'SUBTRACT', value}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', id})
     }
 };
 
