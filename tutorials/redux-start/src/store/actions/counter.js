7279f3f6ec219d9c8324cd092d8692b5126ec3a5
matus dubrava <matusdubrava@gmail.com>
2018-09-18 10:26:08 +0200

    restructured action creators

---
+++ tutorials/redux-start/src/store/actions/counter.js
@@ -0,0 +1,34 @@
+import * as actionTypes from './actionTypes';
+
+export const increment = () => {
+    return {
+        type: actionTypes.INCREMENT
+    };
+};
+
+export const decrement = () => {
+    return {
+        type: actionTypes.DECREMENT
+    };
+};
+
+export const add = (value) => {
+    return {
+        type: actionTypes.ADD,
+        value
+    };
+};
+
+export const subtract = (value) => {
+    return {
+        type: actionTypes.SUBTRACT,
+        value
+    };
+};
+
+export const saveResult = (result) => {
+    return {
+        type: actionTypes.STORE_RESULT,
+        result
+    };
+};
