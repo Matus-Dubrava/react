7671b780c3bbb3128502c947cef6baf8f5d542b4
matus dubrava <matusdubrava@gmail.com>
2018-09-19 23:17:38 +0200

    fixed minor issues

--- tutorials/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ tutorials/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -16,7 +16,7 @@ const sideDrawer = (props) => {
       <Backdrop 
         show={props.open}
         clicked={props.closed} />
-      <div className={attachedClassed.join(' ')}>
+      <div className={attachedClassed.join(' ')} onClick={props.closed}>
         <div className={classes.Logo}>
           <Logo />
         </div>
