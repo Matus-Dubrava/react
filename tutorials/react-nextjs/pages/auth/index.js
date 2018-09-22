5843a085782ac2f422e75789f824ee6d68876215
Sklep <matusdubrava@gmail.com>
2018-09-22 11:05:10 +0200

    added getInitialProps hooks

--- tutorials/react-nextjs/pages/auth/index.js
+++ tutorials/react-nextjs/pages/auth/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-const authIndexPage = () => {
+import User from '../../components/User';
+
+const authIndexPage = (props) => {
   return (
     <div>
-      <h1>The auth page</h1>
+      <h1>The auth page - {props.appName}</h1>
+      <User name="Matus" age={29} />
     </div>
   )
+};
+
+authIndexPage.getInitialProps = (context) => {
+  console.log(context);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: 'Super app (auth)' });
+    }, 1000);
+  });
+
+  return promise;
 };
 
 export default authIndexPage;
--- tutorials/react-nextjs/pages/index.js
+++ tutorials/react-nextjs/pages/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => {
-  return (
-    <div>
-      <h1>The main page</h1>
-      <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-      <button
-        onClick={() => {
-          Router.push('/auth');
-        }}>Go to auth</button>
-    </div>
-  );
+class IndexPage extends Component {
+  static async getInitialProps(context) {
+    console.log(context);
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ appName: 'Super app' });
+      }, 1000);
+    });
+
+    return promise;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The main page of {this.props.appName}</h1>
+        <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+        <button
+          onClick={() => {
+            Router.push('/auth');
+          }}>Go to auth</button>
+      </div>
+    );
+  }
 };
 
-export default indexPage;
+export default IndexPage;
