0404a8e186a6f5e3c5799b26f4bffd285bc0f531
Sklep <matusdubrava@gmail.com>
2018-09-22 22:24:07 +0200

    added css animations

--- tutorials/react-animations/src/components/Backdrop/Backdrop.js
+++ tutorials/react-animations/src/components/Backdrop/Backdrop.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+    const cssClasses = ['Backdrop'];
+    if (props.show) {
+        cssClasses.push('BackdropOpen');
+    } else {
+        cssClasses.push('BackdropClosed');
+    }
+
+    return <div className={cssClasses.join(' ')}></div>
+};
 
 export default backdrop;
--- tutorials/react-animations/src/components/Modal/Modal.js
+++ tutorials/react-animations/src/components/Modal/Modal.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import './Modal.css';
 
-const modal = (props) => (
-    <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
-);
+const modal = (props) => {
+    const cssClasses = ['Modal'];
+    if (props.show) {
+        cssClasses.push('ModalOpen');
+    } else {
+        cssClasses.push('ModalClosed');
+    }
+
+    return (
+        <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>Dismiss</button>
+        </div>
+    );
+};
 
 export default modal;
