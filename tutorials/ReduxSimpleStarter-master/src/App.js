82ec063b4f0599df033f819d117db60241696548
matus dubrava <matusdubrava@gmail.com>
2018-09-27 19:06:23 +0200

    cleared misc stuff

---
+++ tutorials/ReduxSimpleStarter-master/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">My React App</div>
+        );
+    }
+}
+
+export default App;
