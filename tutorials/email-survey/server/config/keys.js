a41347a0d4da431710be0b199b4b64f89407658b
matus dubrava <matusdubrava@gmail.com>
2018-10-07 11:28:06 +0200

    added oauth google strategy setup

---
+++ tutorials/email-survey/server/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    google: {
+        clientID:
+            '676199567841-vkc13538nmgbbmc5vorsa4qgr7i6d9so.apps.googleusercontent.com',
+        clientSecret: 'AEv1_j9qD9XyzGkyYgLXzSu4'
+    }
+};
--- tutorials/email-survey/server/index.js
+++ tutorials/email-survey/server/index.js
@@ -1,23 +1,53 @@
-const express = require("express");
-const log = require("debug")("server:index");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const log = require('debug')('server:index');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const keys = require('./config/keys');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
+
+const googleOptions = {
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret,
+    callbackURL: '/auth/google/redirect'
+};
+
+const googleLogin = new GoogleStrategy(
+    googleOptions,
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile', profile);
+    }
+);
+
+passport.use(googleLogin);
 
 app.use((req, res, next) => {
-  log(`${req.method} - ${req.url}`);
-  next();
+    log(`${req.method} - ${req.url}`);
+    next();
 });
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json({ message: "it is working" });
-});
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get(
+    '/auth/google/redirect',
+    passport.authenticate('google', {
+        successRedirect: '/'
+    })
+);
 
 app.listen(port, () => {
-  log(`Servers started listening on port ${port}`);
+    log(`Servers started listening on port ${port}`);
 });
