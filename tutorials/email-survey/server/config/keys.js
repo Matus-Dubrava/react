b4d5fd0a1c80379647cd9597a18185bae640d14c
matus dubrava <matusdubrava@gmail.com>
2018-10-07 22:03:57 +0200

    added dev/prod env split

--- tutorials/email-survey/server/config/keys.js
+++ tutorials/email-survey/server/config/keys.js
@@ -1,10 +1,5 @@
-module.exports = {
-    google: {
-        clientID:
-            '676199567841-vkc13538nmgbbmc5vorsa4qgr7i6d9so.apps.googleusercontent.com',
-        clientSecret: 'AEv1_j9qD9XyzGkyYgLXzSu4'
-    },
-    mongodbURI: 'mongodb://admin:Admin1@ds225253.mlab.com:25253/email-survey',
-    cookieKey:
-        '!!!!!Some random unGuessable string here!!!!!dsa9d8as8dsa9jdd218d219d128d219d2nd121u2fgb12'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
---
+++ tutorials/email-survey/server/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    google: {
+        clientID: process.end.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    },
+    mongodbURI: process.env.MONGODBURI,
+    cookieKey: process.env.COOKIE_KEY
+};
