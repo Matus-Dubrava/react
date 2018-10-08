7bc7d058320770a17ad6f921e2ad4e13677e2458
matus dubrava <matusdubrava@gmail.com>
2018-10-08 19:27:15 +0200

    added frontend react starter code

--- tutorials/email-survey/server/client/src/index.js
+++ tutorials/email-survey/server/client/src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './containers/App/App';
+import rootReducer from './store/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
