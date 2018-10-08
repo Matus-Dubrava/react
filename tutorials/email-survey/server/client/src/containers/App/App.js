880374c61a4a332a541f66a00b44308b663da185
matus dubrava <matusdubrava@gmail.com>
2018-10-08 20:52:14 +0200

    added initial configuration of react router

--- tutorials/email-survey/server/client/src/containers/App/App.js
+++ tutorials/email-survey/server/client/src/containers/App/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
     render() {
         return (
             <div>
-                <a href="/auth/google">Sign in with Google</a>
+                <Header />
+                <Route path="/" component={Landing} exact />
+                <Route path="/surveys" component={Dashboard} exact />
+                <Route path="/surveys/new" component={SurveyNew} />
             </div>
         );
     }
