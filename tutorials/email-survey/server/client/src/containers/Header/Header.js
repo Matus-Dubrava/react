2094405f799269cedbb079493365b62138353af6
matus dubrava <matusdubrava@gmail.com>
2018-10-09 10:01:31 +0200

    updated redux flow diagram

--- tutorials/email-survey/server/client/src/containers/Header/Header.js
+++ tutorials/email-survey/server/client/src/containers/Header/Header.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../store/actions';
 
 class Header extends Component {
     render() {
@@ -10,6 +13,9 @@ class Header extends Component {
                         <li>
                             <a>Login With Google</a>
                         </li>
+                        <button onClick={this.props.onUserFetch}>
+                            fetch user
+                        </button>
                     </ul>
                 </div>
             </nav>
@@ -17,4 +23,13 @@ class Header extends Component {
     }
 }
 
-export default Header;
+const mapDispatchToProps = dispatch => {
+    return {
+        onUserFetch: () => dispatch(actions.fetchUser())
+    };
+};
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(Header);
--- tutorials/email-survey/server/client/src/store/actions/auth.js
+++ tutorials/email-survey/server/client/src/store/actions/auth.js
@@ -2,30 +2,10 @@ import axios from 'axios';
 
 import { FETCH_USER } from './actionTypes';
 
-export const fetchUser = async () => {
+export const fetchUser = async dispatch => {
     try {
         const response = await axios.get('/api/current_user');
+        console.log(response.data);
+        dispatch({ type: FETCH_USER });
     } catch (err) {}
 };
-
-// const proxy = require('http-proxy-middleware');
-
-// const proxyTable = {
-//     '/auth/google': 'http://localhost:5000/',
-//     '/api/*': 'http://localhost:5000/'
-// };
-
-// const options = {
-//     target: 'http://localhost:4000',
-//     router: proxyTable
-// };
-
-// module.exports = function(app) {
-//     app.use(proxy(options));
-// };
-
-// const proxy = require('http-proxy-middleware');
-
-// module.exports = function(app) {
-//     app.use('/auth/google', proxy({target: 'http://localhost:5000/', changeOrigin: true}));
-// };
