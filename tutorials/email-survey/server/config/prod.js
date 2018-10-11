7445699137142eb04fe9c21a8b052ca919d44655
matus dubrava <matusdubrava@gmail.com>
2018-10-11 16:06:10 +0200

    added stripe react module and stripe API keys to the backend

--- tutorials/email-survey/server/config/prod.js
+++ tutorials/email-survey/server/config/prod.js
@@ -4,5 +4,7 @@ module.exports = {
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
     },
     mongodbURI: process.env.MONGODBURI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
