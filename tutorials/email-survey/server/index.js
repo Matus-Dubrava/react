2b6e858d02df696a17f950bf8c8923c29543c155
matus dubrava <matusdubrava@gmail.com>
2018-10-12 14:34:57 +0200

    fixed broken production code

--- tutorials/email-survey/server/index.js
+++ tutorials/email-survey/server/index.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(
-            path.resolve(__dirname__, 'client', 'build', 'index.html')
+            path.resolve(__dirname, 'client', 'build', 'index.html')
         );
     });
 }
