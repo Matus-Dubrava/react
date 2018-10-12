cd0300c29fd4efff12f364114df72c4cfc1dddc7
matus dubrava <matusdubrava@gmail.com>
2018-10-12 14:02:16 +0200

    added heroku postbuild script and express static file serving

--- tutorials/email-survey/server/index.js
+++ tutorials/email-survey/server/index.js
@@ -43,6 +43,22 @@ app.use(passport.session());
 app.use('/auth', authRouter);
 app.use('/api', apiRouter);
 
+// run this code only in production environment
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like main.js file, or main.css file
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html if
+    // it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname__, 'client', 'build', 'index.html')
+        );
+    });
+}
+
 app.listen(port, () => {
     log(`Servers started listening on port ${port}`);
 });
